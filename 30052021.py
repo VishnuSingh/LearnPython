2594cd81d59462141221cf8e27cc8d3186e03203
vis <vishnu@censhare.com>
2021-06-02 17:04:26 +0530

    [30052021] (vis) - added task 1

---
+++ 30052021.py
@@ -0,0 +1,10 @@
+#Program to write my name that stored in a variable and printing it on console including reversing it.
+
+#storing my name in 'name' variable
+name = "Vishnu"
+#printing it on console
+print(name)
+#slicing the string to make it in reverse order. 
+#we know that -1 is the index of last character of the string.
+#so here we are printing it from start to end in step of -1 (means last)
+print(name[::-1])
