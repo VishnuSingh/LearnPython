b322c7190d32e387182f2c8ad6aa72741b00513d
vis <vishnu@censhare.com>
2021-06-06 13:18:26 +0530

    [06062021]  - added reverse method of list

--- PracticeTasks/06062021_1_Lists.py
+++ PracticeTasks/06062021_1_Lists.py
@@ -9,7 +9,14 @@ print(friendsList[1:6:3])
 
 #we can also do the reverse of this list (as we alredy did for string for my name) "Vishnu" into "unhsiV"
 print(friendsList[::-1])
+#list is also provide a method reverse() to reverse the list elements
+#The reverse() method does not return any value but reverse the given object from the list. Like if we try to do it like
+  #print(friendsList.reverse()) then it will not print anyting and provide 'None' as result.
+print("Reverse function")
+friendsList.reverse()
+print(friendsList)
+
 
 #change an element let say "Raj" into "Rajeev" here
 friendsList[3] = "Rajeev"
-print(friendsList)
+#print(friendsList)
