c4e57ca4ba2e1834a3d1d36ac62b287239daf27b
vis <vishnu@censhare.com>
2021-06-06 12:49:54 +0530

    [06062021_1] - Print list of friend and reverse it

---
+++ PracticeTasks/06062021_1_Lists.py
@@ -0,0 +1,11 @@
+#lists
+friendsList = ["Amar","Pravesh","Prayag","Raj","Vish","Love","DK","Girish"]
+#printing the 3 element of the list
+print(friendsList[3:4])
+
+#list can also be slice with elements as we already did for string. Like
+#print element index 1 in a step of 3 till the element 6, It should print "Pravesh", "Vish" but not Girish as the index is out of range of index 6.
+print(friendsList[1:6:3]) 
+
+#we can also do the reverse of this list (as we alredy did for string for my name) "Vishnu" into "unhsiV"
+print(friendsList[::-1])
