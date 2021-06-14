7ba00fd1e5f5d5f9417ec62c7e7b1aa57dddfd9a
vis <vishnu@censhare.com>
2021-06-14 22:06:38 +0530

    while loop and a simple translatory of vowels into underscore

---
+++ PracticeTasks/translator.py
@@ -0,0 +1,13 @@
+##inputStr = "This is a fruit name Orange. You could enjoy it!"
+def translate(phrase):
+    vowels = "AEIOUaeiou"
+    translated = ''
+    for chars in phrase:
+        if (chars in vowels):
+            translated = translated + '_'
+        else: 
+            translated = translated + chars
+    print(translated)
+
+inputString = input('Enter the string to translate all vowels in underscore:- ')
+print(translate(inputString))
