32327bf0fbabd247322e8d29e54d8ed97fdf3dd4
vis <vishnu@censhare.com>
2021-06-18 22:30:26 +0530

    sorting

---
+++ PracticeTasks/sort_des.py
@@ -0,0 +1,7 @@
+#print number in descending order
+numberSq = [1,2,3,4,5,6,7,8,9,10]
+print(numberSq[::-1])
+
+#print the odd number from a sequence
+numberSq = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+print(numberSq[0::2])
