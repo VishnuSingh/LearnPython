6aeae80f1c136a64faa8fb155a2a980afb269042
vis <vishnu@censhare.com>
2021-06-18 23:48:32 +0530

    import class not working so writing code of class into program itself

--- PracticeTasks/MCQ_Test-1.py
+++ PracticeTasks/MCQ_Test-1.py
@@ -1,19 +1,23 @@
 from Questions import Questions
-
+'''class Questions:
+    def __init__(self, questionPrompt, answer):
+        self.questionPrompt = questionPrompt
+        self.answer = answer
+'''
 questionPrompts = [
-    "1. Capital of India: \n(a) Jaipur\n(b) Delhi\(c)Mumbai",
-    "2. PM of India: \n(a) N Modi\n(b) R Gandhi\(c)A Kejri",
-    "3. Indian currency: \n(a) Dollar\n(b) Euro\(c)Rupees",
-    "4. Current year: \n(a) 2021\n(b) 2020\(c)2022",
-    "5. Current Month: \n(a) May\n(b) June\(c)July"
+    "1. Capital of India: \n(a) Jaipur\n(b) Delhi\n(c)Mumbai\n",
+    "2. PM of India: \n(a) N Modi\n(b) R Gandhi\n(c)A Kejri\n",
+    "3. Indian currency: \n(a) Dollar\n(b) Euro\n(c)Rupees\n",
+    "4. Current year: \n(a) 2021\n(b) 2020\n(c)2022\n",
+    "5. Current Month: \n(a) May\n(b) June\n(c)July\n"
 ]
 
 questions = [
-    Questions(questionPrompts[0], b),
-    Questions(questionPrompts[0], a),
-    Questions(questionPrompts[0], c),
-    Questions(questionPrompts[0], a),
-    Questions(questionPrompts[0], b)
+    Questions(questionPrompts[0], 'b'),
+    Questions(questionPrompts[1], 'a'),
+    Questions(questionPrompts[2], 'c'),
+    Questions(questionPrompts[3], 'a'),
+    Questions(questionPrompts[4], 'b')
 ]
 
 def runTest():
@@ -22,7 +26,7 @@ def runTest():
         answer = input(question.questionPrompt)
         if answer == question.answer:
             score += 1
-    print("You score " + str(score) + "out of" + str(len(questions)))
+    print("You score " + str(score) + " out of " + str(len(questions)))
 
 runTest()
 
